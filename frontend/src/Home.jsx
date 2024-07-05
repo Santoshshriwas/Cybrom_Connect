2152aca5f132d27bf6a857381d7ee20c1f54893c
Shubham <shivu00soni@gmail.com>
2024-07-05 15:00:33 +0530

    Courses page by Shubham

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,7 +3,7 @@ import Landing from "./component/Landing"
 
 const Home=()=>{
 return(<>
-<h1>dicdkbcdc</h1>
+<h1>Platform For Every One </h1>
 
 <Landing />
 
---
+++ frontend/src/component/Courses.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Courses = () => {
+  return (
+    <>
+    <div id='heading'>All Courses</div>
+    <h1>List of courses</h1>
+
+    </>
+  )
+}
+
+export default Courses
